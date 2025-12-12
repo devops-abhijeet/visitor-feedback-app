63e6f063cb963122f8ddd5b4b0b49139cd565a13
abhi <abhijeetkamble14092000@gmail.com>
2025-12-12 16:11:18 +0530

    on

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.get('/feedbacks', (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server Running at http://localhost:${PORT}`));
